d3c7ffc69e334d44c6e27d16b9aa20390ee99516
Vladislav Kartashov <kartashovladislav@gmail.com>
2023-08-26 22:29:52 +0300

    Ktor 2.3.3, Koin DI

--- src/main/kotlin/software/darkmatter/comment/service/comment/CommentService.kt
+++ src/main/kotlin/software/darkmatter/comment/service/comment/CommentService.kt
@@ -1,7 +1,5 @@
 package software.darkmatter.comment.service.comment
 
-import software.darkmatter.comment.service.comment.Comment
-
 interface CommentService {
 
     suspend fun list(): List<Comment>
